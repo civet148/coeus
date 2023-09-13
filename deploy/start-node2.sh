3c8534080e8cf4c9567cf2549faac0af3acbfd12
libin <civet148@126.com>
2023-09-13 17:46:20 +0800

    fix node deploy scripts

--- deploy/start-node2.sh
+++ deploy/start-node2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 IMAGE_URL='coeus-node:latest'
-CONTAINER_NAME=coeus2
+CONTAINER_NAME=coeus-node2
 DATA_DIR=/data/coeus/node2
 
 # p2p port 26656
@@ -10,7 +10,7 @@ DATA_DIR=/data/coeus/node2
 # app api port 1317
 # app grpc port 9090
 # app grpc web port 9091
-docker run --net=host -p 26656:26656 26657:26657 -p 26658:26658 -p 26660:26660 -p 1317:1317 -p 9090:9090 -p 9091:9091 \
+docker run --net=host -p 26656:26656 -p 26657:26657 -p 26658:26658 -p 26660:26660 -p 1317:1317 -p 9090:9090 -p 9091:9091 \
        -v $DATA_DIR:/root/.coeus \
        --restart always \
        --name $CONTAINER_NAME -d $IMAGE_URL coeusd start
